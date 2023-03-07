c6a995eb34fca9b6eab28b4b047b741a45ef6358
arunkoffeekode <arunkoffeekodes599@gmail.com>
2023-03-07 17:20:41 +0530

    7 march 2023 uploaded

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+(function ($) {
+  "use strict";
+
+    // jquery ready start fixed top nav sticky
+    $(document).ready(function() {
+      // jQuery code
+      ///////////////// fixed menu on scroll for desctop
+      if ($(window).width() > 992) {
+        var navbar_height = $('.navbar').outerHeight();
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > 200) {
+            $('.navbar-wrap').css('height', navbar_height + 'px');
+            $('#navbar_top').addClass("fixed-top").fadeIn();
+            $('#navbar_top').removeClass("d-none");
+    
+          } else {
+            $('#navbar_top').addClass("d-none");
+            $('#navbar_top').removeClass("fixed-top").fadeOut();
+            $('.navbar-wrap').css('height', 'auto');
+          }
+        });
+      } // end if
+    }); // jquery end sticky
+
+})
+  (jQuery);
